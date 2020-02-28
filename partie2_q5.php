5b5a240ed86cd652f26d492bf74aa0a5436b30cd
Krell Lucas <lucaskrell@hotmail.fr>
2020-02-28 10:48:41 +0100

    q5

---
+++ partie2_q5.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as DB;
+
+$db = new DB();
+$db->addConnection(parse_ini_file('src/conf/conf.ini'));
+
+$db->setAsGlobal();
+$db->bootEloquent();
+
+$r = \mobileorm\models\Game::select('id', 'name','deck')->chunk(500, function($r) {
+    foreach($r as $value) {
+        echo 'id : '.$value->id.'<br>';
+        echo 'nom : '.$value->name.'<br>';
+        echo 'deck : '.$value->deck.'<br>';
+        echo '<br>';
+    }
+});
