d867af945ba824dad9d2e03cb57f17abcd4b8961
Flachag <flavien.chagras6@etu.univ-lorraine.fr>
2020-03-06 08:56:31 +0100

    Question 4 seance 2

--- TD/seance1/partie2_q1.php
+++ TD/seance1/partie2_q1.php
@@ -4,14 +4,14 @@ require_once __DIR__ . '/vendor/autoload.php';
 use Illuminate\Database\Capsule\Manager as DB;
 
 $db = new DB();
-$db->addConnection(parse_ini_file('src/conf/conf.ini'));
+$db->addConnection(parse_ini_file('../../src/conf/conf.ini'));
 
 $db->setAsGlobal();
 $db->bootEloquent();
 
-$r = \mobileorm\models\Platform::select('id', 'name')->where("install_base", ">=", 10000000)->get();
+$r = \mobileorm\models\Game::select('id', 'name')->where('name', 'LIKE', '%mario%')->get();
 
-foreach ($r as $value){
+foreach($r as $value) {
     echo 'id : '.$value->id.'<br>';
     echo 'nom : '.$value->name.'<br>';
     echo '<br>';
---
+++ seance2_q4.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Illuminate\Database\Capsule\Manager as DB;
+use mobileorm\models\Game;
+
+$db = new DB();
+$db->addConnection(parse_ini_file('src/conf/conf.ini'));
+
+$db->setAsGlobal();
+$db->bootEloquent();
+
+echo '<pre>';
+
+$games = Game::where("name", "like","%mario%")->get();
+
+foreach ($games as $g) {
+
+    echo $g->name . "  --  ";
+    $ogr = $g->original_game_ratings;
+    //  if(isset($ogr["name"]))
+    if ($ogr[0]["name"] != null) {
+        echo $ogr[0]["name"] . "<br>";
+    } else echo "Pas de notation !</not> <br>";
+}
+
