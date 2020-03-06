224c17f502d7ce0182174984d541ecedd26dd21c
Krell Lucas <lucaskrell@hotmail.fr>
2020-03-06 10:12:50 +0100

    fix

--- TD/seance2/seance2_q3.php
+++ TD/seance2/seance2_q3.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '../../../vendor/autoload.php';
 
 use Illuminate\Database\Capsule\Manager as DB;
-use mobileorm\models\Game;
+use mobileorm\models\Company;
 
 $db = new DB();
 $db->addConnection(parse_ini_file('../../src/conf/conf.ini'));
