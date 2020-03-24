122a7f85a0e8083f3f90e76b916e2dc2d54ca69a
Jules Sayer <jules.sayer@protonmail.com>
2020-03-24 11:18:38 +0100

    Partie 1

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager as DB;
+use mobileorm\controllers\ApiController;
+use Slim\App;
+
+require('vendor/autoload.php');
+
+$db = new DB();
+$db->addConnection(parse_ini_file('src/conf/conf.ini'));
+$db->setAsGlobal();
+$db->bootEloquent();
+
+$config = [
+    'settings' => [
+        'displayErrorDetails' => 1
+    ]
+];
+
+$app = new App($config);
+
+$app->get('/api/games/{id:[0-9]+}', ApiController::class . ":game")->setName('game');
+
+$app->run();
---
+++ src/controllers/ApiController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace mobileorm\controllers;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use mobileorm\models\Game;
+use Slim\Http\Request;
+use Slim\Http\Response;
+
+class ApiController {
+    static $error = ['error' => true, 'message' => "Une erreur s'est produite."];
+
+    public function game(Request $request, Response $response, array $args): Response {
+        try {
+            $game = Game::findOrFail($args['id']);
+            $response = $response->withJson($game);
+        } catch(ModelNotFoundException $e) {
+            $response = $response->withJson(static::$error, 404);
+        }
+        return $response;
+    }
+
+}
